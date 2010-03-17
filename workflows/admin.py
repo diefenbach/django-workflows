6578c28e06d50e0eee17b807d857c1efe0e7ed86
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-03-17 10:50:23 +0100

    Added StateObjectRelation to admin

--- workflows/admin.py
+++ workflows/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from workflows.models import State
 from workflows.models import StateInheritanceBlock
 from workflows.models import StatePermissionRelation
+from workflows.models import StateObjectRelation
 from workflows.models import Transition
 from workflows.models import Workflow
 from workflows.models import WorkflowObjectRelation
@@ -20,6 +21,7 @@ admin.site.register(Workflow, WorkflowAdmin)
 
 admin.site.register(State)
 admin.site.register(StateInheritanceBlock)
+admin.site.register(StateObjectRelation)
 admin.site.register(StatePermissionRelation)
 admin.site.register(Transition)
 admin.site.register(WorkflowObjectRelation)
